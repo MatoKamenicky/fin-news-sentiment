3c7179d87b92b8401f4bd419edeaaa0ada98595d
Matej Kamenický <maty.kamenicky@gmail.com>
2025-08-26 22:51:03 +0200

    sp500 data

--- web_app.py
+++ web_app.py
@@ -2,9 +2,15 @@ import write2db as db
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import yfinance as yf
 
+# Data from DB
 query = 'SELECT id, source, headline, sentiment, sentiment_score, scraped FROM headlines;'
 df = db.read_db(query)
+
+# Stock market data
+sp500 = yf.Ticker("^GSPC")
+sp500_hist = sp500.history(period="1mo").reset_index()
 
 # Sidebar menu
 # st.sidebar.title("📌 Menu")
@@ -29,10 +35,12 @@ st.plotly_chart(fig)
 daily_sentiment = df.groupby(df['scraped'].dt.date)['sentiment_score'].mean().reset_index()
 daily_sentiment.columns = ['date', 'avg_sentiment_score']
 
-fig = px.line(daily_sentiment, x='date', y='avg_sentiment_score',
-                title='Average Sentiment Over Time', markers=True)
+fig = px.line(daily_sentiment, x='date', y='avg_sentiment_score', title='Average Sentiment Over Time', markers=True)
 st.plotly_chart(fig)
 
+# S&P 500
+fig = px.line(sp500_hist, x="Date", y="Close", title="S&P500 Closing Price (Last 1 Month)", markers=True)
+st.plotly_chart(fig)
 
 # Latest Headlines
 st.subheader("Latest Headlines")
